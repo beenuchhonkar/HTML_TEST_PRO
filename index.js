a799795bcbe6b6828541be33641c15f4545a1718
beenuchhonkar <98656731+beenuchhonkar@users.noreply.github.com>
2022-01-29 21:47:59 +0530

    test code

---
+++ index.js
@@ -0,0 +1,41 @@
+(()=>{
+    // self calling function
+    alert("Hello Beenu, we are loading the page");
+    var promptvalue=prompt("Enter any value");
+    
+
+    const doubleValue=(val)=>{
+        return val*2;
+    }
+
+    alert("you entered: "+ doubleValue(promptvalue));
+    
+})();
+
+/* how to write a function in javascript */
+/*
+    function function_name(){
+
+
+    }
+
+    const function_name=function(){
+        
+    }
+
+    const function_name=()=>{
+
+    }
+
+*/
+
+/* how to use if else */
+/*
+ var a=0, b=1;
+    if(a==b){
+        console.log("both are equal");
+    }else{
+        console.log("both are unique");
+    }
+
+*/
